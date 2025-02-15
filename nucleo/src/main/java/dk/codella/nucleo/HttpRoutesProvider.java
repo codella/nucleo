27be629e1f0ee71d3281dddcdedb04e0b9f98262
Mauro Codella <mauro.codella@gmail.com>
2025-02-15 19:40:28 +0100

    wip

--- nucleo/src/main/java/dk/codella/nucleo/HttpRoutesProvider.java
+++ nucleo/src/main/java/dk/codella/nucleo/HttpRoutesProvider.java
@@ -1,4 +1,6 @@
 package dk.codella.nucleo;
 
 public interface HttpRoutesProvider extends Runnable {
+  // TODO: Find a Weld idiomatic way to discover resources that register routes on Vert.x's Route
+  // without using this interface
 }
--- nucleo/src/main/java/dk/codella/nucleo/ResteasyResource.java
+++ nucleo/src/main/java/dk/codella/nucleo/ResteasyResource.java
@@ -1,4 +1,5 @@
 package dk.codella.nucleo;
 
 public interface ResteasyResource {
+  // TODO: Find a Weld idiomatic way to discover RESTEasy resources without using this interface
 }
